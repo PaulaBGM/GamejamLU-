84801fea0ba5ac229d47d2413490235b5e7a50db
PaulaBGM <borrasgarciap@gmail.com>
2025-07-07 14:57:42 +0200

    Empezando minijuego barrer

---
+++ Assets/Scripts/PlayerBroomController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerBroomController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool onBroom = false;
+
+    public float broomSpeed = 8f;
+    public Transform broomMountPoint;
+
+    private GameObject broomObject;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (onBroom)
+        {
+            float moveX = Input.GetAxisRaw("Horizontal");
+            float moveY = Input.GetAxisRaw("Vertical");
+            Vector2 movement = new Vector2(moveX, moveY).normalized;
+
+            rb.linearVelocity = movement * broomSpeed;
+
+            if (broomObject != null)
+            {
+                broomObject.transform.position = broomMountPoint.position;
+            }
+        }
+    }
+
+    public void MountBroom(GameObject broomPrefab, ParticleSystem particles)
+    {
+        if (onBroom) return;
+
+        if (particles != null)
+        {
+            Instantiate(particles, transform.position, Quaternion.identity).Play();
+        }
+
+        broomObject = Instantiate(broomPrefab, broomMountPoint.position, Quaternion.identity);
+        broomObject.transform.SetParent(transform);
+
+        onBroom = true;
+        Debug.Log("¡Montado en la escoba!");
+    }
+
+    // ✅ Método que faltaba
+    public bool IsOnBroom()
+    {
+        return onBroom;
+    }
+}
